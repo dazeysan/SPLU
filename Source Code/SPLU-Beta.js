b173407108d1388e8293bde6165d5bb2972002d6
dazeysan <dazeysan@gmail.com>
2015-04-04 08:42:35 -0700

    Add Sorting functions to sort plays
    And maybe improve other sorting

--- Source Code/SPLU-Beta.js
+++ Source Code/SPLU-Beta.js
@@ -269,6 +269,38 @@
     }
   }
 
+  function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+      sortOrder = -1;
+      property = property.substr(1);
+    }
+    return function (a,b) {
+      var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+      return result * sortOrder;
+    }
+}
+
+  function dynamicSortMultiple() {
+    /*
+     * save the arguments object as it will be overwritten
+     * note that arguments object is an array-like object
+     * consisting of the names of the properties to sort by
+     */
+    var props = arguments;
+    return function (obj1, obj2) {
+      var i = 0, result = 0, numberOfProperties = props.length;
+      /* try getting a different result from 0 (equal)
+       * as long as we have extra properties to compare
+       */
+      while(result === 0 && i < numberOfProperties) {
+        result = dynamicSort(props[i])(obj1, obj2);
+        i++;
+      }
+      return result;
+    }
+  }
+
   function getGameID(){
     var metas=document.getElementsByTagName('meta');
     for(i=0;i<metas.length;i++){
@@ -1000,14 +1032,19 @@
     }else{
       var tmpHTML="";
       var display=true;
+      var tmpSort=[];
       tmpHTML='<div id="SPLU.PlaysTable" style="display:table;">';
       for(key in SPLUplayData[tmpUser]){
         if(key=="total"||SPLUplayData[tmpUser][key].attributes.date.value=="1452-04-15"){
           continue;
         }
-        tmpPlayId=key;
-        tmpPlayDate=SPLUplayData[tmpUser][key].attributes.date.value;
-        tmpPlayGame=SPLUplayData[tmpUser][key].getElementsByTagName("item")[0].attributes.name.value;
+        tmpSort.push({id:key,date:SPLUplayData[tmpUser][key].attributes.date.value});
+      }
+      tmpSort.sort(dynamicSortMultiple("-date", "id"));
+      for(i=0;i<tmpSort.length;i++){
+        tmpPlayId=tmpSort[i]["id"];
+        tmpPlayDate=SPLUplayData[tmpUser][tmpPlayId].attributes.date.value;
+        tmpPlayGame=SPLUplayData[tmpUser][tmpPlayId].getElementsByTagName("item")[0].attributes.name.value;
         tmpHTML+='<div id="SPLU.Plays-'+tmpPlayId+'" style="display:table-row;"><div style="display:table-cell;">'+tmpPlayDate+' - <a href="javascript:{void(0);}" onClick="javascript:{loadPlay('+tmpPlayId+');}">'+tmpPlayGame+'</a></div></div>';
       }
       tmpHTML+='</div>';
