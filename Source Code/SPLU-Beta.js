62aa091b285459579b001d9ced1874c64f97ddc6
dazeysan <dazeysan@gmail.com>
2015-06-28 21:56:28 -0700

    Not Not Filtering

--- Source Code/SPLU-Beta.js
+++ Source Code/SPLU-Beta.js
@@ -1201,7 +1201,7 @@
       if(filtertype=="location"){
         for(i=0;i<plays.length;i++){
           if(lines[l].value.slice(0,1)=="!"){
-            if(SPLUplayData[user][plays[i].id].getAttribute("location").toLowerCase().indexOf(lines[l].value.toLowerCase())==-1){
+            if(SPLUplayData[user][plays[i].id].getAttribute("location").toLowerCase().indexOf(lines[l].value.slice(1).toLowerCase())==-1){
               plays[i].matches++;
             }
           } else if(SPLUplayData[user][plays[i].id].getAttribute("location").toLowerCase().indexOf(lines[l].value.toLowerCase())>-1){
