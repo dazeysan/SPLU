078f79b8b87373620553737c814f1cbc9409be15
katsup-sauce <katsup-sauce@users.noreply.github.com>
2015-07-14 08:46:23 -0700

    Made YYYY font be 8pt for both fields

--- Source Code/SPLU-Beta.js
+++ Source Code/SPLU-Beta.js
@@ -2335,7 +2335,7 @@
       }
       
       if(filter=="daterange"){
-        tmpHTML+='Begin:<input type="text" style="font-size:small;width:70px;" placeholder="YYYY-MM-DD" name="SPLU.PlaysFiltersLine" data-SPLU-FilterType="begindate" onKeyPress="eventFilterLineEnter(event)"/> End:<input type="text" placeholder="YYYY-MM-DD" name="SPLU.PlaysFiltersLine2" data-SPLU-FilterType="enddate" onKeyPress="eventFilterLineEnter(event)" style="width:70px;"/>';
+        tmpHTML+='Begin:<input type="text" style="font-size:8pt;width:70px;" placeholder="YYYY-MM-DD" name="SPLU.PlaysFiltersLine" data-SPLU-FilterType="begindate" onKeyPress="eventFilterLineEnter(event)"/> End:<input type="text" style="font-size:8pt;width:70px;" placeholder="YYYY-MM-DD" name="SPLU.PlaysFiltersLine2" data-SPLU-FilterType="enddate" onKeyPress="eventFilterLineEnter(event)"/>';
       }
       
       if(filter=="excludeexpansions"){
