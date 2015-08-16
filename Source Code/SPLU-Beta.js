680808f1240c325fd50938ca52e4a7bd986b1ead
katsup-sauce <katsup-sauce@users.noreply.github.com>
2015-08-15 23:18:24 -0700

    Update SPLU-Beta.js

--- Source Code/SPLU-Beta.js
+++ Source Code/SPLU-Beta.js
@@ -2853,7 +2853,7 @@ function getStatsLocations(tmpUser){
     }
   }
   
-  function makeSentence(){
+    function makeSentence(){
     if(!SPLU.Settings.SummaryTextField.Visible){return;}
     document.getElementById('SPLU.SummaryTextField').style.maxWidth=document.getElementById('SPLUwindow').clientWidth-40+"px";
     document.getElementById('SPLU.SummaryTextField').style.display="block";
@@ -2862,9 +2862,19 @@ function getStatsLocations(tmpUser){
     var sentence="";
     sentence="You are logging ";
     if(document.getElementById('quickplay_quantity99').value==1){
-      sentence+="a play of ";  
-    }else{
-      sentence+=document.getElementById('quickplay_quantity99').value;  
+      sentence+="a ";
+	if(PlayerCount==1&&NumOfPlayers!=1){
+	  sentence+="solo ";
+    }
+    if(document.getElementById('quickplay_quantity99').value==1){
+     sentence+="play of ";  
+      }
+	}
+else{
+      sentence+=document.getElementById('quickplay_quantity99').value;
+	if(PlayerCount==1&&NumOfPlayers!=1){
+	  sentence+=" solo ";
+    }
       sentence+=" plays of ";
     }
     sentence+=document.getElementById('q546e9ffd96dfc').value;
@@ -2894,9 +2904,7 @@ function getStatsLocations(tmpUser){
     }else{
       sentence+=".";
     }
-    if(PlayerCount==1&&NumOfPlayers!=1){
-      sentence+=" There was only one player.";
-    }
+    
     if(PlayerCount>1){
       sentence+=" There were ";
       sentence+=PlayerCount;
@@ -2929,13 +2937,17 @@ function getStatsLocations(tmpUser){
       if(document.getElementById('quickplay_quantity99').value==1){
         sentence+=" The game lasted ";
         sentence+=document.getElementById('quickplay_duration99').value;
+
         if(document.getElementById('quickplay_duration99').value<5){
           sentence+=" whole";
         }
         if(document.getElementById('quickplay_duration99').value==1){
           sentence+=" minute.";
         }else{
-          sentence+=" minutes.";
+		if(document.getElementById('quickplay_duration99').value<5){
+          sentence+=" whole";
+          }
+		sentence+=" minutes.";
         }
       }  
       if(document.getElementById('quickplay_quantity99').value>1){
@@ -2943,9 +2955,11 @@ function getStatsLocations(tmpUser){
         sentence+=document.getElementById('quickplay_duration99').value;
         sentence+=" minutes.";
       }
+if(PlayerCount==1&&NumOfPlayers!=1){
+      sentence+=" Nicely done!";
+    }
     }
     document.getElementById('SPLU.SummaryTextField').innerHTML=sentence;
-  }
   
   function hideSentence(){
     document.getElementById('SPLU.SummaryTextField').style.display="none";
