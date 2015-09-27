8c1744ac4b18c6345445c62c1bb1647e90fe0019
katsup-sauce <katsup-sauce@users.noreply.github.com>
2015-09-26 22:22:43 -0700

    Update SPLU-Alpha.js
    Replaced the Edit Players icon with a combination of two from Font Awesome.

--- Source Code/SPLU-Alpha.js
+++ Source Code/SPLU-Alpha.js
@@ -287,7 +287,7 @@
         +'<div style="display:table-row;">'
           +'<div class="BRcells">'
             +'Players:<a href="javascript:{void(0);}" onClick="javascript:{showHidePlayers(false,\'reset\');}" id="SPLU.SavedNamesBtn" style="padding-left:1px;"><span id="SPLU.SavedNamesButtonIconCollapse" style="display:inline-block;"><img src="https://raw.githubusercontent.com/dazeysan/SPLU/master/Images/arrow_up.png"></span><span id="SPLU.SavedNamesButtonIconExpand" style="display:none;"><img src="https://raw.githubusercontent.com/dazeysan/SPLU/master/Images/arrow_down.png"></span></a>'
-            +'<a href="javascript:{void(0);}" onClick="javascript:{showPlayersPane(\'button\');}" id="showPlayersPaneBtn" style="padding-right:5px;"><img src="https://raw.githubusercontent.com/dazeysan/SPLU/master/Images/players.png"></a>'
+            +'<a href="javascript:{void(0);}" onClick="javascript:{showPlayersPane(\'button\');}" id="showPlayersPaneBtn" style="padding-right:5px;"><span class="fa-stack"><i class="fa fa-bars fa-stack-2x fa-pull-right" style="color:gray"></i><i class="fa fa-user fa-stack-2x fa-pull-left" style="color:black"></i></span></a>'
             +'<div style="display:inline;" id="SPLU.PlayerFilters">'
               +'<select id="SPLU.SelectFilter" onChange="javascript:{setFilter(\'choose\');}"></select>'
             +'</div>'
