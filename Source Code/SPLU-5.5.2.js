8ffe840c0a56b691f930b9dbfa837ab4bd6bd794
dazeysan <dazeysan@gmail.com>
2016-01-08 16:53:30 -0800

    Pushing 5.5.3
    With quick workaround for groups bug

--- Source Code/SPLU-5.5.2.js
+++ Source Code/SPLU-5.5.2.js
@@ -1,4 +1,4 @@
-// SPLU 5.5.3 Beta
+// SPLU 5.5.2 Beta
 
     //Check if they aren't on a BGG site and alert them to that fact.
     if(window.location.host.slice(-17)!="boardgamegeek.com" &&  window.location.host.slice(-17)!="videogamegeek.com" && window.location.host.slice(-11)!="rpggeek.com" && window.location.host.slice(-6)!="bgg.cc" && window.location.host.slice(-10)!="geekdo.com"){
@@ -12,7 +12,7 @@
     //var LoggedInAs = document.getElementsByClassName('menu_login')[0].childNodes[3].childNodes[1].innerHTML;
     //Check if the user is logged in to BGG, throw an error if not
     //if(LoggedInAs==""){alert("You aren't logged in.");throw new Error("You aren't logged in.");}
-    var SPLUversion="5.5.3";
+    var SPLUversion="5.5.2";
 
     var SPLU={};
     var SPLUplayId="";
@@ -1866,9 +1866,6 @@
       if(player.attributes.new.value==1){
         tmpNew="checked";
       }
-    } else if(SPLU.Players[player]===undefined){
-      console.log(player+" does not exist.");
-      return;
     } else if(player!=-1){
       tmpName=decodeURIComponent(SPLU.Players[player].Name);
       tmpUser=decodeURIComponent(SPLU.Players[player].Username);
