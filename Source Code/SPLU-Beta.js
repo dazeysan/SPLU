eebee711364fd375cabc3ac44c51d539461e6e84
dazeysan <dazeysan@gmail.com>
2016-01-08 16:37:41 -0800

    Testing a workaround to bad player names

--- Source Code/SPLU-Beta.js
+++ Source Code/SPLU-Beta.js
@@ -1,4 +1,4 @@
-// SPLU 5.5.2 Beta
+// SPLU 5.5.3 Beta
 
     //Check if they aren't on a BGG site and alert them to that fact.
     if(window.location.host.slice(-17)!="boardgamegeek.com" &&  window.location.host.slice(-17)!="videogamegeek.com" && window.location.host.slice(-11)!="rpggeek.com" && window.location.host.slice(-6)!="bgg.cc" && window.location.host.slice(-10)!="geekdo.com"){
@@ -12,7 +12,7 @@
     //var LoggedInAs = document.getElementsByClassName('menu_login')[0].childNodes[3].childNodes[1].innerHTML;
     //Check if the user is logged in to BGG, throw an error if not
     //if(LoggedInAs==""){alert("You aren't logged in.");throw new Error("You aren't logged in.");}
-    var SPLUversion="5.5.2";
+    var SPLUversion="5.5.3";
 
     var SPLU={};
     var SPLUplayId="";
@@ -1866,7 +1866,7 @@
       if(player.attributes.new.value==1){
         tmpNew="checked";
       }
-    } else if(player!=-1){
+    } else if(player!=-1 && SPLU.Players[player]!==undefined){
       tmpName=decodeURIComponent(SPLU.Players[player].Name);
       tmpUser=decodeURIComponent(SPLU.Players[player].Username);
       tmpColor=decodeURIComponent(SPLU.Players[player].Color);
