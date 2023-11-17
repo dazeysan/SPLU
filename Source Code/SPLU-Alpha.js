7261212ca656f66285ed1cbefad6ea9ca4818d85
dazeysan <dazeysan@gmail.com>
2023-11-16 22:06:36 -0500

    Fixing BBcode changes

--- Source Code/SPLU-Alpha.js
+++ Source Code/SPLU-Alpha.js
@@ -4888,6 +4888,7 @@
   }
   
   function generateBBcode() {
+    document.getElementById("SPLU.BBstatus").innerHTML="Fetching Cover Art";
     tmpList=[];
     for(i=0; i<SPLUlistOfPlays.length; i++){
       tmpList.push(SPLUplayData[document.getElementById("SPLU.PlaysLogger").value][SPLUlistOfPlays[i].id].objectid);
@@ -4910,7 +4911,7 @@
     tmpListUnique=tmpList.filter(onlyUnique);
     //console.log(tmpListUnique);
     for(t=0; t<tmpListUnique.length; t++){
-      tmpBBcode+="[imageid="+SPLUimageData[tmpListUnique[t]].item.imageid+" "+tmpObj.arguments.size+" inline]";
+      tmpBBcode+="[imageid="+SPLUimageData[tmpListUnique[t]].imageid+" "+tmpObj.arguments.size+" inline]";
     }
     console.log(tmpBBcode);
     document.getElementById("SPLU.BBstatus").innerHTML="<input type='button' onclick='javascript:{navigator.clipboard.writeText(tmpBBcode);}' value='Click to copy BBcode to clipboard'>";
